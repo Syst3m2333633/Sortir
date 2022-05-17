74666b2e273816a7195f28456cc5731eaac7dfac
KRS-7 <christophe.leclere2022@campus-eni.fr>
2022-05-17 16:13:43 +0200

    - Creation de la vue "nouvel utilisateur" (creationUser.html.twig) - Creation de l'entite Participants (Participants.php) - Creation du formulaire Nouvel Utilisateur (CreationUserType) - Creation du controleur Participants (ParticipantController) - MAJ CSS du formulaire inscription

---
+++ src/Form/CreationUserType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participants;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreationUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo', TextType::class, [
+                'label'=> 'Pseudonyme'
+            ])
+            ->add('nom', TextType::class, [
+                'label'=> 'Nom'
+            ])
+            ->add('prenom', TextType::class, [
+                'label'=> 'Prenom'
+            ])
+            ->add('Telephone')
+            ->add('mail', TextType::class, [
+                'label'=> 'Email'
+            ])
+            ->add('password', TextType::class, [
+                'label'=> 'Mot de passe'
+            ])
+
+            ->add('confirmation', TextType::class, [
+                'label'=> 'Confirmation'
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participants::class,
+        ]);
+    }
+}
