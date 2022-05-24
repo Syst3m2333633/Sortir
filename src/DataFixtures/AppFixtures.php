52a694306946cb3f3555ea661526a996117035cb
Alain Catheland <alain.catheland@gmail.com>
2022-05-24 17:04:08 +0100

    Hydradation des tables de la base de données

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,7 +3,11 @@
 namespace App\DataFixtures;
 
 use App\Entity\Campus;
+use App\Entity\Etat;
+use App\Entity\Lieu;
+use App\Entity\Participant;
 use App\Entity\Sortie;
+use App\Entity\Ville;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -11,13 +15,89 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        //CAMPUS
         $campus = new Campus();
         $campus->setNom("Un campus de test");
+
         $manager->persist($campus);
 
         $sortie = new Sortie();
         $sortie->setCampus($campus);
 
         $manager->flush();
+
+        //PARTICIPANT
+        $participant = new Participant();
+        $participant->setIdentifiant("gdlj44");
+        $participant->setNom("Delajungle");
+        $participant->setPrenom("George");
+        $participant->setTelephone("0102030405");
+        $participant->setEmail("g@dlj.fr");
+        $participant->setPassword("test1234");
+        $participant->setConfirmation("test1234");
+        $participant->setAdministrateur(false);
+        $participant->setActif(true);
+
+        $manager->persist($participant);
+
+        $sortie = new Sortie();
+        $sortie->setParticipant($participant);
+
+        $manager->flush();
+
+        //ETAT
+        $etat = new Etat();
+        $etat->setLibelle("Créée");
+
+        $manager->persist($etat);
+
+        $sortie = new Sortie();
+        $sortie->setEtat($etat);
+
+        $manager->flush();
+
+        //LIEU
+        $lieu = new Lieu();
+        $lieu->setNom("Tour Eiffel");
+        $lieu->setRue("Boulevard de la république");
+        $lieu->setLatitude(48.51);
+        $lieu->setLongitude(2.17);
+
+        $manager->persist($lieu);
+
+        $sortie = new Sortie();
+        $sortie->setLieu($lieu);
+
+        $manager->flush();
+
+        //VILLE
+        $ville = new Ville();
+        $ville->setNom("Paris");
+        $ville->setCodePostal(75000);
+
+        $manager->persist($ville);
+
+        $sortie = new Sortie();
+        $sortie->setVille($ville);
+
+        $manager->flush();
+
+        //Sortie
+        $sortie = new Sortie();
+        $sortie->setNom("Eiffel");
+        $sortie->setDateHeureDebut("31/08/2022");
+        $sortie->setDuree(5);
+        $sortie->setDateLimiteInscription("21/08/2022");
+        $sortie->setNbInscriptionsMax(50);
+        $sortie->setInfosSortie("blablabla");
+        $sortie->setEtat("Créée");
+
+        $manager->persist($sortie);
+
+        $sortie = new Sortie();
+        $sortie->setSortie($sortie);
+
+        $manager->flush();
+
     }
 }
